ccaefa04c6e43f72764ff9f937bf37fd5e18b15b
disterics <disterics@wojeshun.net>
2015-06-02 15:34:07 -0700

    It seems ack-and-a-half has been abandoned. So switching to using silver searcher(ag).
    Update personal packages to exclude helm-ack and include ag.

--- src/personal/personal-packages.el
+++ src/personal/personal-packages.el
@@ -1,5 +1,9 @@
 ;;; Auto install the following packages
 
+;;; Code:
 
 ;; install the helm search packages
-(prelude-require-packages '(helm-ag helm-ack))
+(prelude-require-packages '(ag helm-ag))
+
+(provide 'personal-packages)
+;;; personal-packages.el ends here
